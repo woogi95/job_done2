e04df4a5016970e53dc7b38181dac5449d31da3c
woogi95 <sdw951216@gmail.com>
2025-02-13 16:53:04 +0900

    백업

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./App";
+import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
+import App from "./App";
 import "./index.css";
 
 const queryClient = new QueryClient({
@@ -15,11 +14,9 @@ const queryClient = new QueryClient({
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </RecoilRoot>
-  </React.StrictMode>,
+  <RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </RecoilRoot>,
 );
