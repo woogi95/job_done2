4baebbb8dbd4a6b5034ba2f48eea64d7193416ba
blendy9702 <lch99206@naver.com>
2025-03-20 17:49:21 +0900

    [fixed] chat done

--- src/pages/MyPage/MyMessage.jsx
+++ src/pages/MyPage/MyMessage.jsx
@@ -57,7 +57,7 @@ function ContactUs() {
         // console.log("roomId가 없습니다.");
         return;
       }
-      ws = new WebSocket(`wss://job-done.r-e.kr:52340/chat/${roomId}`);
+      ws = new WebSocket(`/chat/${roomId}`);
 
       ws.onopen = () => {
         // console.log("웹소켓 연결 성공!");
